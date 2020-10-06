bc3a0424943881337f459d1b64a12ea9cce296bf
Simon Lee <great.simonlee@gmail.com>
2020-10-06 08:39:27 -0400

    initial files created

---
+++ resources/js/data.js
@@ -0,0 +1,7 @@
+const mainAboutMeContents = `Hi! Everyone!! I am Seunghoon Simon Lee.
+
+I'm so excited to be able to learn about web development and expand various networking through the Columbia University coding boot camp. I became interested in making business development and investment strategies by utilizing business data in various fields.
+
+As managing various international businesses in our company's portfolio, I was able to see the distinctive busisness strategies that can save the entire company or drag the business into a unescapable trap. Nowadays, all the companies are establising their special business strategy based on their previous datasets. However, only few of them can reach the right decision by utilizing the right datasets.
+
+So, my final goal is to develop a web-based corporate data managemenet system so that all my client will be able to survive even in an unprecedented situation in the future.`;
