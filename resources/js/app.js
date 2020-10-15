6ddac97dfe815b822cb76a70604fa83e37a9105f
Seunghoon Lee <seunghoonlee@seunghoons-mbp.home>
2020-10-15 07:38:43 -0400

    Print button name done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
 "use strict";
+
+var currentSEEOption;
+const aboutMeSEEBtn = document.getElementsByClassName("aboutMeSEEBtn");
+const aboutMeSkillDiv = document.getElementById("aboutMeSkill");
+const aboutMeExperienceDiv = document.getElementById("aboutMeExperience");
+const aboutMeEducationDiv = document.getElementById("aboutMeEducation");
+
+function setAboutMeSEEOptionClick () {
+    for (let i=0; i<aboutMeSEEBtn.length; i++) {
+        aboutMeSEEBtn[i].addEventListener("click", (e) => {
+            currentSEEOption = e.target.innerText;
+            console.log(currentSEEOption);
+        })
+    }
+}
+
+// Add Paging Function with currentSeeOption
+
+setAboutMeSEEOptionClick();
+
+// function setCurrentSEEOption () {
+//     document.getElementById("aboutMeSkill").addEventListener("click", changeCurrentSEEOption(currentSEEOption))
+// }
+
+// function changeCurrentSEEOption(button) {
+//   currentSEEOption = button.
+// }
+
+// for (i=0, i<BtnLength)
+// console.log(document.getElementsByClassName("aboutMeSEEBtn"))
